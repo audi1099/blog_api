e32155ca3de0b267d30228eb575835e4ee9005a4
Vitali Kaliutchyk <vitalikaliutchyk@gmail.com>
2025-03-13 00:16:37 +0300

    Create settings register router Post Comments

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from posts.views import PostViewSet, CommentViewSet
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     #    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/auth/', include('rest_framework.urls')),
 ]
